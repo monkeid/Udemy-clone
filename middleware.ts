a713e98283e951fbc1e13cd8f6317791288d794d
monkeid <jackoraro0@gmail.com>
2024-08-30 09:48:37 +0300

    update

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publishableKey: "pk_test_d2VsbC1jb3ctNDEuY2xlcmsuYWNjb3VudHMuZGV2JA",
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
